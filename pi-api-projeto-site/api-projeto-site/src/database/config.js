0ab8a78793939435b6d6a3cdb1bd30d9e6e9e4e4
lisacastil <elisa.castilho.marques@gmail.com>
2021-11-22 14:04:30 -0300

    alterando index

--- pi-api-projeto-site/api-projeto-site/src/database/config.js
+++ pi-api-projeto-site/api-projeto-site/src/database/config.js
@@ -2,27 +2,27 @@ var mysql = require("mysql2");
 var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
-var sqlServerConfig = {
-    user: "XXXXX",
-    password: "XXXXX",  
-    database: "XXXXX",
-    server: "XXXXX",
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-    },
-    options: {  
-        encrypt: true, // for azure
-    }
-}
+// var sqlServerConfig = {
+//     user: "XXXXX",
+//     password: "XXXXX",  
+//     database: "XXXXX",
+//     server: "XXXXX",
+//     pool: {
+//         max: 10,
+//         min: 0,
+//         idleTimeoutMillis: 30000
+//     },
+//     options: {  
+//         encrypt: true, // for azure
+//     }
+// }
 
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
     user: "root",
     password: "password",
-    database: "acquatec",
+    database: "folheando",
 };
 
 function executar(instrucao) {
